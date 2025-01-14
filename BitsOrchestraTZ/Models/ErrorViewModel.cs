0d552ae48e6837e172912f02f6f9d25db742d7d9
Prikhod322 <vovkaprikhod@gmail.com>
2025-01-14 18:26:33 +0200

    Add project files.

---
+++ BitsOrchestraTZ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BitsOrchestraTZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
